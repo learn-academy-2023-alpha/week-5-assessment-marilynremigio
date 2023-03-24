7e09a2f1e933c9e7d5b74a84072709c59082bf61
Marilyn Remigio <marilynremigio@gmail.com>
2023-03-24 15:31:24 -0600

    completed week 5 assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -14,39 +14,178 @@
 
 // a) Create a test with expect statements using the variables provided.
 
-const secretCodeWord1 = "Lackadaisical"
-// Expected output: "L4ck4d41s1c4l"
-const secretCodeWord2 = "Gobbledygook"
-// Expected output: "G0bbl3dyg00k"
-const secretCodeWord3 = "Eccentric"
-// Expected output: "3cc3ntr1c"
+describe("secretCode", () => {
+    it("returns a string with a coded message", () => {
+        const secretCodeWord1 = "Lackadaisical" // Expected output: "L4ck4d41s1c4l"
+        expect(secretCode(secretCodeWord1)).toEqual("L4ck4d41s1c4l")
+        const secretCodeWord2 = "Gobbledygook" // Expected output: "G0bbl3dyg00k"
+        expect(secretCode(secretCodeWord2)).toEqual("G0bbl3dyg00k")
+        const secretCodeWord3 = "Eccentric" // Expected output: "3cc3ntr1c"
+        expect(secretCode(secretCodeWord3)).toEqual("3cc3ntr1c")
+    })
+})
+
+// ReferenceError: secretCode is not defined
 
 // b) Create the function that makes the test pass.
+
+    // Pseudo Code:
+        // Create a function named secretCode that takes in a string as an argument
+        // Input: a string with str as the parameter name
+        // Create an object named code with key:value pairs of the letter and corresponding number
+        // Use the .replace method with RegEx to replace the vowels (aeio) with its assigned numbers
+        // Adding the g (global) flag and the i (case) flag allows the function to perform case-insensitive replacements at multiple indexes
+        // Output: a coded string
+
+const code = {
+        a: 4,
+        e: 3,
+        i: 1,
+        o: 0,
+        A: 4,
+        E: 3,
+        I: 1,
+        O: 0
+}
+const secretCode = (str) => {
+       return str.replace(/[aeio]/gi, (vowel) => code[vowel])
+}
+     
+const secretCodeWord1 = "Lackadaisical"
+const secretCodeWord2 = "Gobbledygook"
+const secretCodeWord3 = "Eccentric"
+console.log(secretCode(secretCodeWord1)) // -> L4ck4d41s1c4l
+console.log(secretCode(secretCodeWord2)) // -> G0bbl3dyg00k
+console.log(secretCode(secretCodeWord3)) // -> 3cc3ntr1c
+
+//   secretCode
+// ✓ returns a string with a coded message (4 ms)
+
+// Test Suites: 1 passed, 1 total
+// Tests:       1 passed, 1 total
+
+
+
+
 
 // --------------------2) Create a function that takes in an array of words and a single letter and returns an array of all the words containing that particular letter.
 
 // a) Create a test with expects statement using the variable provided.
 
-const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
+describe("containingThatLetter", () => {
+    it("takes in an array of words and a single letter and returns an array of all the words containing that particular letter", () => {
+        const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
+        const letterA = "a" // Expected output: ["Mango", "Apricot", "Peach"]
+        expect(containingThatLetter(fruitArray, letterA)).toEqual(["Mango", "Apricot", "Peach"])
+        const letterE = "e" // Expected output: ["Cherry", "Blueberry", "Peach"]
+        expect(containingThatLetter(fruitArray, letterE)).toEqual(["Cherry", "Blueberry", "Peach"])
+    })
+})
 
-const letterA = "a"
-// Expected output: ["Mango", "Apricot", "Peach"]
-const letterE = "e"
-// Expected output: ["Cherry", "Blueberry", "Peach"]
+//  ReferenceError: containingThatLetter is not defined
 
 // b) Create the function that makes the test pass.
+
+    // Pseudo code:
+        // Create a function named containingThatLetter that takes in two arguments: an array and a string
+        // The parameters will be named array and letter
+        // Input: an array of words and a string of one letter
+        // Use HOF .filter to iterate over the array and check if any word contains the given letter using accessor method .includes()
+        // Return the word in the filtered array if it includes the letter as is OR || the letter capitalized using built-in method .toUpperCase()
+        // Output: one array that only includes the words containing the particular letter given(whether its lowercase or uppercase)
+
+const containingThatLetter = (array, letter) => {
+    return array.filter(value => {
+        return value.includes(letter) || value.includes(letter.toUpperCase())
+    })
+}
+
+const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
+const letterA = "a" 
+const letterE = "e" 
+console.log(containingThatLetter(fruitArray, letterA)) // ->  [ 'Mango', 'Apricot', 'Peach' ]
+console.log(containingThatLetter(fruitArray, letterE)) // ->  [ 'Cherry', 'Blueberry', 'Peach' ]
+
+// containingThatLetter
+//   ✓ takes in an array of words and a single letter and returns an array of all the words containing that particular letter
+
+// Test Suites: 1 passed, 1 total
+// Tests:       2 passed, 2 total
+
+
+
+
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a "full house". A full house is exactly one pair and one three of a kind.
 
 // a) Create a test with expect statements using the variable provided.
 
-const hand1 = [5, 5, 5, 3, 3]
-// Expected output: true
-const hand2 = [5, 5, 3, 3, 4]
-// Expected output: false
-const hand3 = [5, 5, 5, 5, 4]
-// Expected output: false
-const hand4 = [7, 2, 7, 2, 7]
-// Expected output: true
+describe("fullHouse", () => {
+    it("determines whether or not the array is a full house", () => {
+        const hand1 = [5, 5, 5, 3, 3] // Expected output: true
+        expect(fullHouse(hand1)).toEqual(true)
+        const hand2 = [5, 5, 3, 3, 4] // Expected output: false
+        expect(fullHouse(hand2)).toEqual(false)
+        const hand3 = [5, 5, 5, 5, 4] // Expected output: false
+        expect(fullHouse(hand3)).toEqual(false)
+        const hand4 = [7, 2, 7, 2, 7] // Expected output: true
+        expect(fullHouse(hand4)).toEqual(true)
+    })
+})
+
+// ReferenceError: fullHouse is not defined
 
 // b) Create the function that makes the test pass.
+    // Pseudo code: 
+        // Create a function named fullHouse that takes in an array as an argument 
+        // Input: an array of 5 numbers
+        // Create a copy of the given array using the .slice() method so we can compare the values at each index without modifying the original array (Visual aid: copyArray = [x,x,x,x,x] )
+        // If one value is found at 3 different indexes and another value is found at the remaining 2 indexes, then the array has a full house (Visual aid: [x,x,x,y,y] = full house )
+        // Iterate over the array using a for loop with the start at i=0 and the end at i < array.length
+        // Since we want to take the first value and check if its found 2 other times in the array, we will execute multiple lines of code on the first iteration before adding our increment of i++ to the loop
+        // Set a variable num1 to copyArray[0] (Visual aid: [num1,x,x,x,x] )
+        // Remove the value of the current iteration from copyArray using .splice(i,1). (Visual aid: [x,x,x,x] )  Use conditional if to check that num1 can be found in the spliced array by using .includes(value) method with num1 as the value parameter. (Visual aid: if num1 is found somewhere here [x,num1,x,x] then its found twice in the array)
+        // Now, remove the current first occurence of num1 in copyArray so we can check if its found a third time
+        // Use .splice(copyArray.indexOf(num1),1) on copyArray (Visual aid: [x,x,x] )
+        // Check if num1 appears a third time by using conditional if on copyArray.includes(num1) 
+        // If it does, then we repeat the previous .slice() step to give us an array with the last 2 values ([x,x])
+        // Now we can check if the remaining two numbers are equal. Set num2 to the current first index copyArray[0] and use conditional if to check if num2 === copyArray[1] the value at the next index
+        // If it is true, then return true. If at any point, the conditionals aren't true, then come out of the loop and run the iteration on the next index. If a full house is not found, return false
+        // Output: true or false based on if the array elements conclude to a full house 
+
+const fullHouse = (array) => { // EXAMPLE hand4 = [7, 2, 7, 2, 7] 
+    copyArray = array.slice() // makes copy of array so copyArray = [7, 2, 7, 2, 7]
+    for(i=0; i < array.length;){ // for loop starting at 0 index, ending at last index
+        const num1 = copyArray[0] // set num1 = first value = 7 
+        if(copyArray.splice(i, 1).includes(num1)){ // check if copyArray splice first index [2, 7, 2, 7] includes 7
+            copyArray.splice(copyArray.indexOf(num1), 1) // deletes the next index of 7 -> [2, 2, 7]
+            if(copyArray.includes(num1)){ // checks if [2, 2, 7] includes 7 for a third time
+                copyArray.splice(copyArray.indexOf(num1), 1) // removes the third 7 -> [2, 2]
+                const num2 = copyArray[0] // set num2 = first index = 2
+                if(num2 === copyArray[1]){ // check if num2 === next index 
+                    return true // returns true if num1 is found 3 times and num2 is found 2 times
+                }
+            }
+        }else{
+            i++
+        }
+    }
+    return false
+}
+
+const hand1 = [5, 5, 5, 3, 3]
+const hand2 = [5, 5, 3, 3, 4]
+const hand3 = [5, 5, 5, 5, 4]
+const hand4 = [7, 2, 7, 2, 7]
+
+console.log(fullHouse(hand1))
+console.log(fullHouse(hand2))
+console.log(fullHouse(hand3))
+console.log(fullHouse(hand4))
+
+
+// fullHouse
+// ✓ determines whether or not the array is a full house (2 ms)
+
+// Test Suites: 1 passed, 1 total
+// Tests:       3 passed, 3 total
--- interview-questions.md
+++ interview-questions.md
@@ -8,44 +8,44 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What does calling super do in a Ruby class?
 
-Your answer:
+Your answer: In ruby, there is something called class inheritance, which expresses a relationship between classes. Calling a super means a subclass or child class wants to pull information or inherit from a superclass, also known as the parent class. Superclasses have a collection of similiar data and when a subclass with unique data needs to pull that information, we call on super.
 
-Researched answer:
+Researched answer: Super is a keyword in Ruby which calls on the Superclass implementation of the method we're currently working with. It passes information from the original method to the current one or from the Parent class to a Child class. How it works is that the Subclass has an initialize method which will invoke the Superclass and take in the same arguments in order to create a new instance of the argument, but with specific information of the subclass. To inherit we use the < symbol as such: Subclass < Parent.
 
 2. What is a gem?
 
-Your answer:
+Your answer: A gem is a library of code for Ruby. Similar to YARN being a package manager for dependencies, there is a Ruby package manager for gems.
 
-Researched answer:
+Researched answer: A gem is a library that holds code, documentation, and specifications on the gem. With each gem, it has a name, version, and platform. The package manager for gems is simply called RubyGems. All gems are organized in the same format which shows the lib directory that contains the code snippets, the test directory which contains tests dependent on the framework a developer is using, and usually a Rakefile which automates tests and generates code.
 
 3. What is a relational database? Are there other kinds of databases?
 
-Your answer:
+Your answer: A relational database is a table of information where separate pieces of data are related to each other. A visual example would be similiar to an Excel spreadsheet with columns and rows. I believe there are many different kinds of databases, but I'm most familiar with relational databases.
 
-Researched answer:
+Researched answer: A relational database was designed in the 1970s and most commonly uses SQL or Structured Query Language to use CRUD operations on data. CRUD stands for creating, reading, updating, and deleting. In a relational database, information is stored in a table with columns and rows. The data in each row has a primary key to uniquely identify it. Separate tables can associate to one another using a foreign key. This relationship is the has_many, belongs_to relationship. Other kinds of databases include hierarchical, non-relational, and object-oriented databases. Hierarchical databases are similar to a family tree, where there are "parent" objects and one or more "child" objects beneath it. A NoSQL database is one that is non-relational. Examples are MongoDB and Redis. Another type of database is an object-oriented database, where objects are stored and managed on a server's disk. OOP is the most common programming structure, so having these databases makes programming fast and powerful.
 
 4. What are primary keys? Why are they important?
 
-Your answer:
+Your answer: A primary key is a unique identifier of data in a model, typically a number assigned to the data within a list. Primary keys are important so that we can locate information and keep data organized. 
 
-Researched answer:
+Researched answer: Having primary keys gives a way for data in a relational database to be identified. Every row in a table will have a unique ID or primary key to ensure that data with the same or similiar information doesn't get mixed up and can be located separately by their primary key. For example if we had a table model of the Alpha Cohort and one column was Name, we'd have about 20 rows in the Name column, each with a student's personal name. Along with the name is an assigned primary key which would be numbers 1-20. Each name can only be assigned one primary key and this is helpful if there were 2 Marilyn's in the class. One Marilyn R. and a Marilyn M. To differentiate the Marilyn's, we would refer to their primary keys.
 
 5. What are the HTTP verbs? What is each verb's corresponding CRUD action?
 
-Your answer:
+Your answer: The HTTP verbs are get, post, put, patch, and delete. CRUD is the developer's philosophy to include each of these elements in a successful application: Create, Read, Update, and Delete. A get request corresponds to reading or displaying content. A post request corresponds to creating new content. A put request is updating and replacing content. A patch request is updating and editing content, and lastly, a delete request corresponds to removing content. 
 
-Researched answer:
+Researched answer: The most commonly used HTTP verbs are post, get, put, patch, and delete. To initiate a post request, we are creating a new resource. Once sucessful, we will get a return HTTP status of 201. The get request action is used to retrieve content or read a representation of a resource. Without errors, we will see XML or JSON returned with an HTTP response code of 200. If an error was to occur, we'd get a 404 or 400 status which means the resource was either not found or came from a bad request. A put request gives updating capabilites to replace content in a resource. Similarly, the patch request enables modifcations or edits to content, which updates the resources with only the changes required. Lastly, the delete request removes a resource. Deleting sucessfully will return a 200 HTTP status.
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
-1. RESTful routes:
+1. RESTful routes: A set of standards used for different types of languages to create clean, working, and reusable routes. It's purpose is to contain HTTP verbs and CRUD actions together.
 
-2. Model validations:
+2. Model validations: Ensures than only clean working data is saved in a database, which is important because full-stack applications rely on data. Validations protect the data to make sure anything not clean does not get saved.
 
-3. Params:
+3. Params: Short for parameters, to allow developers to include extra information without a URL
 
-4. ERB:
+4. ERB: An extension that stands for embedded Ruby, which is a file structure that enables developers to code in HTML and Ruby. 
 
-5. API:
+5. API: Stands for Application Programming Interface which allows two application systems to communicate through requests and responses and transmits data without a view
